d7517325352305e2b36c18064e16deacfe84a62e
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-01-31 13:09:35 +0700

    Chapter 5: Click Events

--- src/Content.js
+++ src/Content.js
@@ -6,12 +6,27 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3)
         return name[int]
     }
+
+    const handleClick = () => {
+        console.log("you click it")
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
     
 	return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => handleClick2('yonan')}>Click it</button>
+            <button onClick={(e) => handleClick3(e)}>Click it</button>
         </main>
     )
 };
